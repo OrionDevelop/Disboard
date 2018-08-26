a9c0cf6e5bb8ee39083825646465667d77b21de7
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-08-26 16:19:03 +0900

    refactor(mastodon): ReSharper

--- Source/Disboard.Mastodon/MastodonClient.cs
+++ Source/Disboard.Mastodon/MastodonClient.cs
@@ -4,15 +4,6 @@ namespace Disboard.Mastodon
 {
     public class MastodonClient : ClientBase
     {
-        public AccountsClient Account { get; }
-        public AppsClient Apps { get; }
-        public AuthClient Auth { get; }
-        public BlocksClient Blocks { get; }
-        public CustomEmojisClient CustomEmojis { get; }
-        public EndorsementsClient Endorsements { get; }
-        public InstanceClient Instance { get; }
-        public NotificationsClient Notifications { get; }
-
         internal string Domain { get; }
 
         public MastodonClient(string domain) : base($"https://{domain}", "2.0")
@@ -28,5 +19,18 @@ namespace Disboard.Mastodon
             Instance = new InstanceClient(this);
             Notifications = new NotificationsClient(this);
         }
+
+        // ReSharper disable UnusedAutoPropertyAccessor.Global
+
+        public AccountsClient Account { get; }
+        public AppsClient Apps { get; }
+        public AuthClient Auth { get; }
+        public BlocksClient Blocks { get; }
+        public CustomEmojisClient CustomEmojis { get; }
+        public EndorsementsClient Endorsements { get; }
+        public InstanceClient Instance { get; }
+        public NotificationsClient Notifications { get; }
+
+        // ReSharper restore UnusedAutoPropertyAccessor.Global
     }
 }
