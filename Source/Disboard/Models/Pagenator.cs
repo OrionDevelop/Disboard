45ae6756f1ff69cd18389caa8d6d8e4d3210899d
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-12 08:43:28 +0900

    fix(core): Change model structure

--- Source/Disboard/Models/Pagenator.cs
+++ Source/Disboard/Models/Pagenator.cs
@@ -8,104 +8,68 @@ namespace Disboard.Models
     /// <summary>
     ///     RFC 5988 Web Linking Object
     /// </summary>
-    public interface IPagenator<T>
+    public class Pagenator<T> : ApiResponse
     {
+        internal ClientBase Client { get; set; }
+
         /// <summary>
         ///     First page
         /// </summary>
-        string First { get; set; }
+        public string First { get; set; }
 
         /// <summary>
         ///     Next page
         /// </summary>
-        string Next { get; set; }
+        public string Next { get; set; }
 
         /// <summary>
         ///     Previous page
         /// </summary>
-        string Prev { get; set; }
+        public string Prev { get; set; }
 
         /// <summary>
         ///     Last page
         /// </summary>
-        string Last { get; set; }
-
-        /// <summary>
-        ///     Instance of <see cref="ClientBase" />
-        /// </summary>
-        ClientBase Client { set; }
+        public string Last { get; set; }
 
         /// <summary>
         ///     Query first page
         /// </summary>
-        Task<T> FirstAsync();
-
-        /// <summary>
-        ///     Query next page
-        /// </summary>
-        Task<T> NextAsync();
-
-        /// <summary>
-        ///     Query previous page
-        /// </summary>
-        Task<T> PrevAsync();
-
-        /// <summary>
-        ///     Query last page
-        /// </summary>
-        Task<T> LastAsync();
-    }
-
-    /// <summary>
-    ///     IPagenator implementation
-    /// </summary>
-    public class Pagenator<T> : IPagenator<T>
-    {
-        /// <inheritdoc />
-        public string First { get; set; }
-
-        /// <inheritdoc />
-        public string Next { get; set; }
-
-        /// <inheritdoc />
-        public string Prev { get; set; }
-
-        /// <inheritdoc />
-        public string Last { get; set; }
-
-        /// <inheritdoc />
-        public ClientBase Client { get; set; }
-
-        /// <inheritdoc />
         public async Task<T> FirstAsync()
         {
             if (string.IsNullOrWhiteSpace(First))
                 throw new InvalidOperationException();
-            return await Client.GetAsync<T>(First, null).Stay();
+            return await Client.GetAsync<T>(First).Stay();
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Query next page
+        /// </summary>
         public async Task<T> NextAsync()
         {
             if (string.IsNullOrWhiteSpace(Next))
                 throw new InvalidOperationException();
-            return await Client.GetAsync<T>(Next, null).Stay();
+            return await Client.GetAsync<T>(Next).Stay();
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Query previous page
+        /// </summary>
         public async Task<T> PrevAsync()
         {
             if (string.IsNullOrWhiteSpace(Prev))
                 throw new InvalidOperationException();
-            return await Client.GetAsync<T>(Prev, null).Stay();
+            return await Client.GetAsync<T>(Prev).Stay();
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Query last page
+        /// </summary>
         public async Task<T> LastAsync()
         {
             if (string.IsNullOrWhiteSpace(Last))
                 throw new InvalidOperationException();
-            return await Client.GetAsync<T>(Last, null).Stay();
+            return await Client.GetAsync<T>(Last).Stay();
         }
     }
 }
