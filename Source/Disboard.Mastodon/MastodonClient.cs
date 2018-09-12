ac7dcc7a970c1d696fa29e4dbeb1ac1fce1a2fce
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-12 23:11:16 +0900

    feat(mastodon): Add /accounts endpoints

--- Source/Disboard.Mastodon/MastodonClient.cs
+++ Source/Disboard.Mastodon/MastodonClient.cs
@@ -1,14 +1,17 @@
-﻿using Disboard.Mastodon.Clients;
+﻿using System.Collections.Generic;
+
+using Disboard.Mastodon.Clients;
 
 namespace Disboard.Mastodon
 {
-    public class MastodonClient : ClientBase
+    public class MastodonClient : AppClient
     {
         internal string Domain { get; }
 
         public MastodonClient(string domain) : base($"https://{domain}", "2.0")
         {
             Domain = domain;
+            BinaryParameters = new List<string> {"avatar", "header"};
 
             Account = new AccountsClient(this);
             Apps = new AppsClient(this);
