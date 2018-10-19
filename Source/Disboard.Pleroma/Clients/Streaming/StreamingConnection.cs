ed3fe14c8fa2b9e2492a1a9af2fc9b30533cfe39
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-19 21:28:59 +0900

    feat(pleroma): Support Pleroma!

---
+++ Source/Disboard.Pleroma/Clients/Streaming/StreamingConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Disboard.Clients;
+using Disboard.Mastodon.Models.Streaming;
+using Disboard.Models;
+
+using Newtonsoft.Json;
+
+namespace Disboard.Pleroma.Clients.Streaming
+{
+    internal class StreamingConnection : WebSocketStreamingConnection<Pleroma.PleromaClient>
+    {
+        private readonly string _endpoint;
+        private readonly IEnumerable<KeyValuePair<string, object>> _parameters;
+
+        public StreamingConnection(Pleroma.PleromaClient client, string endpoint, IEnumerable<KeyValuePair<string, object>> parameters) : base(client)
+        {
+            _endpoint = endpoint;
+            _parameters = parameters;
+        }
+
+        public IObservable<IStreamMessage> Connect()
+        {
+            return Connect(_endpoint, _parameters);
+        }
+
+        protected override bool IsMatchRequestAndResponse(object request, IStreamMessage response)
+        {
+            return true;
+        }
+
+        protected override IStreamMessage ParseData(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                return null;
+
+            var data = JsonConvert.DeserializeObject<WebSocketFrame>(message);
+            switch (data.Event)
+            {
+                case "update":
+                    return JsonConvert.DeserializeObject<StatusMessage>(data.Payload);
+
+                case "notification":
+                    return JsonConvert.DeserializeObject<NotificationMessage>(data.Payload);
+
+                case "delete":
+                    return new DeleteMessage {Id = long.Parse(data.Payload)};
+
+                case "filters_changed":
+                    return new FilterChangedMessage();
+
+                default:
+                    throw new ArgumentOutOfRangeException(data.Event);
+            }
+        }
+    }
+}
