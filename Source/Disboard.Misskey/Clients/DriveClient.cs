eaa1842e01766dc3206bb260e6676ae9f4d4982d
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-21 02:14:21 +0900

    feat(misskey): Add /drive endpoints

--- Source/Disboard.Misskey/Clients/DriveClient.cs
+++ Source/Disboard.Misskey/Clients/DriveClient.cs
@@ -19,5 +19,38 @@ namespace Disboard.Misskey.Clients
             Folders = new FoldersClient(client);
         }
 
+        public async Task<IEnumerable<File>> FilesAsync(string folderId = null, string type = null, int? limit = null, int? sinceId = null, int? untilId = null)
+        {
+            var parameters = new List<KeyValuePair<string, object>>();
+            parameters.AddIfValidValue("folderId", folderId);
+            parameters.AddIfValidValue("type", type);
+            parameters.AddIfValidValue("limit", limit);
+            parameters.AddIfValidValue("sinceId", sinceId);
+            parameters.AddIfValidValue("untilId", untilId);
+
+            return await PostAsync<IEnumerable<File>>("/files", parameters).Stay();
+        }
+
+        public async Task<IEnumerable<Folder>> FoldersAsync(string folderId = null, int? limit = null, int? sinceId = null, int? untilId = null)
+        {
+            var parameters = new List<KeyValuePair<string, object>>();
+            parameters.AddIfValidValue("folderId", folderId);
+            parameters.AddIfValidValue("limit", limit);
+            parameters.AddIfValidValue("sinceId", sinceId);
+            parameters.AddIfValidValue("untilId", untilId);
+
+            return await PostAsync<IEnumerable<Folder>>("/folders", parameters).Stay();
+        }
+
+        public async Task<IEnumerable<File>> StreamAsync(string type = null, int? limit = null, int? sinceId = null, int? untilId = null)
+        {
+            var parameters = new List<KeyValuePair<string, object>>();
+            parameters.AddIfValidValue("type", type);
+            parameters.AddIfValidValue("limit", limit);
+            parameters.AddIfValidValue("sinceId", sinceId);
+            parameters.AddIfValidValue("untilId", untilId);
+
+            return await PostAsync<IEnumerable<File>>("/stream", parameters).Stay();
+        }
     }
 }
