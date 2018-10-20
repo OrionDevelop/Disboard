f0ffe46deb408cd58b573aa3f7f78e13033a5465
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-20 17:34:29 +0900

    feat(core): Support UnionType to Convert DateTime

---
+++ Source/Disboard/Converters/UnionToDateTimeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Disboard.Converters
+{
+    /// <summary>
+    ///     IsoDateTime, UnixTime, {year, month, day} to DateTime
+    /// </summary>
+    public class UnionToDateTimeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return DateTime.MinValue;
+            if (reader.TokenType == JsonToken.StartObject)
+                return new DateObjectToDateTimeConverter().ReadJson(reader, objectType, existingValue, serializer);
+            var value = reader.Value as string;
+            return long.TryParse(value, out _)
+                ? new UnixTimeToDateTimeConverter().ReadJson(reader, objectType, existingValue, serializer)
+                : new IsoDateTimeConverter().ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
