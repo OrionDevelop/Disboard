e24efb74682cca325079c726b40508146168c1e9
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-23 21:09:42 +0900

    chore(mastodon): fix test

--- Source/Tests/Disboard.Mastodon.Test/Clients/AuthClientTest.cs
+++ Source/Tests/Disboard.Mastodon.Test/Clients/AuthClientTest.cs
@@ -16,12 +16,15 @@ namespace Disboard.Mastodon.Test.Clients
             actual.CheckRecursively();
         }
 
-        [Fact(Skip = "Currently, Using Real Client ID")]
+        [Fact]
         public void AuthorizeUrl()
         {
+            const string clientId = "08b26d89391544984b9a23d95f3a4c91b2f7cdfff7775a7452f7c1cb6c6c048d";
+            var query = $"scope=read%20write%20follow&response_type=code&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&client_id={clientId}";
+
             var scopes = AccessScope.Read | AccessScope.Write | AccessScope.Follow;
             var actual = TestClient.Auth.AuthorizeUrl(Constants.RedirectUriForClient, scopes);
-            actual.Is("https://mastodon.cloud/oauth/authorize?scope=read%20write%20follow&response_type=code&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&client_id=TEST_CLIENT_ID");
+            actual.Is($"https://mastodon.cloud/oauth/authorize?{query}");
         }
     }
 }
