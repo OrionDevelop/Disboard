185bc709c0a2f0b81b2e9cb28f1744b1081fd453
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-23 21:03:24 +0900

    chore(mastodon): Add test tokens

--- Source/Tests/Disboard.Mastodon.Test/MastodonTestClient.cs
+++ Source/Tests/Disboard.Mastodon.Test/MastodonTestClient.cs
@@ -8,6 +8,9 @@ namespace Disboard.Mastodon.Test
 {
     public class MastodonTestClient
     {
+        private const string AccessToken = "TEST_ACCESS_TOKEN";
+        private const string ClientId = "08b26d89391544984b9a23d95f3a4c91b2f7cdfff7775a7452f7c1cb6c6c048d";
+        private const string ClientSecret = "55d24a5f4f480d26fa02d7e96b8b879b0a0853fa6d76c6861272b231f4eee81b";
         protected MastodonClient TestClient { get; }
 
         protected MastodonTestClient()
