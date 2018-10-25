727f57970a140d6a89a3ec373b7abfa164bf8e63
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-25 21:04:22 +0900

    chore(sln): exclude build docs

--- Source/Tests/Disboard.Mastodon.Test/Clients/NotificationsClientTest.cs
+++ Source/Tests/Disboard.Mastodon.Test/Clients/NotificationsClientTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 
-using Disboard.Exceptions;
 using Disboard.Mastodon.Enums;
 using Disboard.Test.Helpers;
 
--- Source/Tests/Disboard.Mastodon.Test/Clients/StatusesClientTest.cs
+++ Source/Tests/Disboard.Mastodon.Test/Clients/StatusesClientTest.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 
-using Disboard.Exceptions;
 using Disboard.Mastodon.Enums;
 using Disboard.Test.Helpers;
 
