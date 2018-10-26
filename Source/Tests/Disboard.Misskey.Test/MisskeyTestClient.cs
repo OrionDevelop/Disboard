999c1f1894d4b9bb8b378b8ac71c1526daf8bc33
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-26 20:02:11 +0900

    chore(ci): Use environment variable in Release

--- Source/Tests/Disboard.Misskey.Test/MisskeyTestClient.cs
+++ Source/Tests/Disboard.Misskey.Test/MisskeyTestClient.cs
@@ -1,12 +1,18 @@
+using System;
+
 using Disboard.Test.Handlers;
 
 namespace Disboard.Misskey.Test
 {
     public class MisskeyTestClient
     {
-        private const string AccessToken = "";
-        private const string ClientSecret = "";
-
+#if DEBUG
+        protected const string AccessToken = "TEST_ACCESS_TOKEN";
+        protected const string ClientSecret = "TEST_CLIENT_SECRET";
+#else
+        protected readonly string AccessToken = Environment.GetEnvironmentVariable("MISSKEY_ACCESS_TOKEN");
+        protected readonly string ClientSecret = Environment.GetEnvironmentVariable("MISSKEY_CLIENT_SECRET");
+#endif
         protected MisskeyClient TestClient { get; }
 
         protected MisskeyTestClient()
