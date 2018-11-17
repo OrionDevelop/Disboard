9f9a10b21e435487b2f85ef08a1f7c3bdaaf3a03
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-11-17 14:17:53 +0900

    refactor(core): +

--- Source/Disboard/Clients/ApiClient.cs
+++ Source/Disboard/Clients/ApiClient.cs
@@ -18,52 +18,52 @@ namespace Disboard.Clients
 
         protected async Task<TR> GetAsync<TR>(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.GetAsync<TR>($"{_base}{endpoint}", parameters).Stay();
+            return await Client.GetAsync<TR>(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<string> GetAsync(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.GetAsync($"{_base}{endpoint}", parameters).Stay();
+            return await Client.GetAsync(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<TR> PostAsync<TR>(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PostAsync<TR>($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PostAsync<TR>(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<string> PostAsync(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PostAsync($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PostAsync(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<TR> PatchAsync<TR>(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PatchAsync<TR>($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PatchAsync<TR>(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<string> PatchAsync(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PatchAsync($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PatchAsync(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<TR> PutAsync<TR>(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PutAsync<TR>($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PutAsync<TR>(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<string> PutAsync(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.PutAsync($"{_base}{endpoint}", parameters).Stay();
+            return await Client.PutAsync(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<TR> DeleteAsync<TR>(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.DeleteAsync<TR>($"{_base}{endpoint}", parameters).Stay();
+            return await Client.DeleteAsync<TR>(_base + endpoint, parameters).Stay();
         }
 
         protected async Task<string> DeleteAsync(string endpoint = "", IEnumerable<KeyValuePair<string, object>> parameters = null)
         {
-            return await Client.DeleteAsync($"{_base}{endpoint}", parameters).Stay();
+            return await Client.DeleteAsync(_base + endpoint, parameters).Stay();
         }
     }
 }
