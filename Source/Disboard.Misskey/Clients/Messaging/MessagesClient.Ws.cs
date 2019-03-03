776a986e918beb3440d99c1ada09fefa5f475e0f
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-03-03 16:19:40 +0900

    feat(misskey): support /messaging endpoints

--- Source/Disboard.Misskey/Clients/Messaging/MessagesClient.Ws.cs
+++ Source/Disboard.Misskey/Clients/Messaging/MessagesClient.Ws.cs
@@ -10,16 +10,23 @@ namespace Disboard.Misskey.Clients.Messaging
     {
         public async Task<Message> CreateWsAsync(string userId, string text = null, string fileId = null)
         {
-            var parameters = new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>("userId", userId)};
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("userId", userId) };
             parameters.AddIfValidValue("text", text);
             parameters.AddIfValidValue("fileId", fileId);
 
             return await SendWsAsync<Message>("/create", parameters).Stay();
         }
 
+        public async Task DeleteWsAsync(string messageId)
+        {
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("messageId", messageId) };
+
+            await SendWsAsync("/delete", parameters).Stay();
+        }
+
         public async Task ReadWsAsync(string messageId)
         {
-            var parameters = new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>("messageId", messageId)};
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("messageId", messageId) };
 
             await SendWsAsync("/read", parameters).Stay();
         }
--- Source/Disboard.Misskey/Clients/Messaging/MessagesClient.cs
+++ Source/Disboard.Misskey/Clients/Messaging/MessagesClient.cs
@@ -12,16 +12,23 @@ namespace Disboard.Misskey.Clients.Messaging
 
         public async Task<Message> CreateAsync(string userId, string text = null, string fileId = null)
         {
-            var parameters = new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>("userId", userId)};
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("userId", userId) };
             parameters.AddIfValidValue("text", text);
             parameters.AddIfValidValue("fileId", fileId);
 
             return await PostAsync<Message>("/create", parameters).Stay();
         }
 
+        public async Task DeleteAsync(string messageId)
+        {
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("messageId", messageId) };
+
+            await PostAsync("/delete", parameters).Stay();
+        }
+
         public async Task ReadAsync(string messageId)
         {
-            var parameters = new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>("messageId", messageId)};
+            var parameters = new List<KeyValuePair<string, object>> { new KeyValuePair<string, object>("messageId", messageId) };
 
             await PostAsync("/read", parameters).Stay();
         }
