dc2e6377b6ada92fd198a7df11f2c364747cd0b5
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-06-30 03:49:41 +0900

    feat(misskey): support rgba format

--- Source/Disboard/Converters/RgbToColorConverter.cs
+++ Source/Disboard/Converters/RgbToColorConverter.cs
@@ -11,7 +11,7 @@ namespace Disboard.Converters
     /// </summary>
     public class RgbToColorConverter : JsonConverter
     {
-        private readonly Regex _color = new Regex(@"rgb\((?<red>\w{1,3}),(?<green>\w{1,3}),(?<blue>\w{1,3})\)", RegexOptions.Compiled);
+        private readonly Regex _color = new Regex(@"rgba?\((?<red>\w{1,3}),(?<green>\w{1,3}),(?<blue>\w{1,3})(,(?<alpha>\w{1,3}))?\)", RegexOptions.Compiled);
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
@@ -31,7 +31,11 @@ namespace Disboard.Converters
 
             var match = _color.Match(color);
             var (r, g, b) = (int.Parse(match.Groups["red"].Value), int.Parse(match.Groups["green"].Value), int.Parse(match.Groups["blue"].Value));
-            return Color.FromArgb(r, g, b);
+            if (string.IsNullOrEmpty(match.Groups["alpha"].Value))
+                return Color.FromArgb(r, g, b);
+
+            var a = int.Parse(match.Groups["alpha"].Value);
+            return Color.FromArgb(a, r, g, b);
         }
 
         public override bool CanConvert(Type objectType)
